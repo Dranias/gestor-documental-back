69f5f152b1ce49aba44bb604b4bc768139b020d2
Dranias <noaydeh@hotmail.com>
2025-09-17 15:17:04 -0600

    Se elimina SSL por que no es necesario en este proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-
 import * as express from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as https from 'https';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, '..', 'ssl', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '..', 'ssl', 'cert.pem')),
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-
-  //Crea la ruta uploads si no existe; carpeta para los merge pdf
+  // 📂 Crea la ruta uploads si no existe (carpeta para los merge pdf)
   const uploadPath = path.join(__dirname, '..', 'uploads');
+
   if (!fs.existsSync(uploadPath)) {
     fs.mkdirSync(uploadPath);
   } else {
     // Si existe, eliminar todos los archivos dentro
     const files = fs.readdirSync(uploadPath);
+
     for (const file of files) {
       const filePath = path.join(uploadPath, file);
+
       // Verificar que sea archivo para evitar borrar subcarpetas accidentalmente
       if (fs.lstatSync(filePath).isFile()) {
         fs.unlinkSync(filePath);
@@ -35,12 +28,17 @@ async function bootstrap() {
     }
   }
 
-  // Configurar el adaptador de WebSocket de NestJS
+  // 🔌 Configurar el adaptador de WebSocket de NestJS
   app.useWebSocketAdapter(new IoAdapter(app));
 
-  // Configurar CORS para la API REST
+  // 🌍 Configurar CORS para la API REST
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://192.168.103.214:5173', 'http://192.168.103.214:1500', 'http://localhost:1500'],
+    origin: [
+      'http://localhost:5173',
+      'http://192.168.103.214:5173',
+      'http://192.168.103.214:1500',
+      'http://localhost:1500',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -49,14 +47,14 @@ async function bootstrap() {
   // ✅ Archivos públicos
   app.use('/pdfs', express.static(path.join(__dirname, '..', 'public', 'pdfs')));
 
+  // ⚙️ Configuración de servidor
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('APP_PORT');
   const HOST = configService.get<string>('APP_HOST');
 
   await app.listen(PORT, HOST, () => {
-    console.log(`Servidor corriendo en https://${HOST}:${PORT}`);
+    console.log(`🚀 Servidor corriendo en http://${HOST}:${PORT}`);
   });
 }
 
 bootstrap();
-
